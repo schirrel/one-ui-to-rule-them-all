5c26430ce2e3fc10003c2cb56857da377a804926
Alan Schio <alanbschio@gmail.com>
2022-09-02 16:12:30 -0300

    feat(lit)(button): finished

--- components/albs-button/src/AlbsButton.ts
+++ components/albs-button/src/AlbsButton.ts
@@ -2,66 +2,106 @@ import { html, css, LitElement } from "lit";
 import { property } from "lit/decorators.js";
 
 export class AlbsButton extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      padding: 25px;
-      color: var(--albs-button-text-color, #000);
-    }
 
-    button {
-      background: royalblue;
-      padding: 8px 10px;
-      border-radius: 16px;
-      border: 1px solid white;
-      color: white;
-      cursor: pointer;
-    }
-    button[type="submit"] {
-      background: goldenrod;
-    }
-
-    button[type="reset"] {
-      background: crimson;
-    }
-
-    button[disabled="true"] {
-      background: gray;
-      cursor: not-allowed;
-      opacity: 0.5;
-    }
-    .albs-button--loading {
-      display: inline-block;
-      padding: 0 8px;
-    }
-    .albs-button--loading:after {
-      content: "   ";
-      width: 10px;
-      display: inline-block;
-      text-align: left;
-      animation: spin 2s linear infinite;
-    }
-    @keyframes spin {
-      0 {
-        content: "   ";
-      }
-      30% {
-        content: ".  ";
-      }
-      60% {
-        content: ".. ";
-      }
-      100% {
-        content: "...";
-      }
-    }
-  `;
+  @property({ type: String }) greeting;
+  @property({ type: String }) planet;
 
   @property({ type: String }) type: "submit" | "reset" | "button" = "button";
   @property({ type: Boolean }) disabled = false;
   @property({ type: Boolean }) loading = false;
 
+  static properties = {
+    greeting: {},
+    planet: {},
+  };
+  // Styles are scoped to this element: they won't conflict with styles
+  // on the main page or in other components. Styling API can be exposed
+  // via CSS custom properties.
+  static styles = css`
+  :host {
+    display: block;
+    padding: 25px;
+    color: var(--albs-button-text-color, #000);
+  }
+
+  button {
+    background: royalblue;
+    padding: 8px 10px;
+    border-radius: 16px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+  }
+  button[type="submit"] {
+    background: goldenrod;
+  }
+
+  button[type="reset"] {
+    background: crimson;
+  }
+
+  button[disabled] {
+    background: gray;
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+  button[disabled="false"] {
+    pointer-events:all
+  }
+  .albs-button--loading {
+    display: inline-block;
+    padding: 0 8px;
+  }
+  .albs-button--loading:after {
+    content: "   ";
+    width: 10px;
+    display: inline-block;
+    text-align: left;
+    animation: spin 2s linear infinite;
+  }
+  @keyframes spin {
+    0 {
+      content: "   ";
+    }
+    30% {
+      content: ".  ";
+    }
+    60% {
+      content: ".. ";
+    }
+    100% {
+      content: "...";
+    }
+  }
+`;
+
+  constructor() {
+    super();
+    this.greeting = 'Hello';
+    this.planet = 'World';
+  }
+
+  render() {
+    console.log(this.type, this.disabled)
+    return html`
+      <button @click=${this.handleClick}
+      ?disabled="${this.disabled}"
+        type="${this.type}"
+        >${this.greeting}
+        <span class="planet">${this.planet}</span>
+        ${this.loading
+        ? html`<span class="albs-button--loading"></span>`
+        : html``}
+
+        disabled? ${this.disabled}
+        ${this.disabled == true ? 'disabled=true' : ''}
+        <slot/>
+      </button>
+    `;
+  }
+
   handleClick(event: Event) {
+    console.log("handleClick")
     event.stopPropagation();
 
     if (this.loading || this.disabled) return;
@@ -75,7 +115,10 @@ export class AlbsButton extends LitElement {
         break;
       case "reset":
         if (form) {
+          console.log(form)
           form.reset();
+        } else {
+          throw "Button reset must be inside a form"
         }
 
         break;
@@ -89,19 +132,7 @@ export class AlbsButton extends LitElement {
         );
         break;
     }
-  }
-  render() {
-    return html`
-      <button
-        disabled="${this.disabled}"
-        type="${this.type}"
-        @click=${this.handleClick}
-      >
-        ${this.loading
-          ? html`<span class="albs-button--loading"></span>`
-          : html``}
-        <slot></slot>
-      </button>
-    `;
+
+    this.planet = this.planet === 'World' ? 'Mars' : 'World';
   }
 }
